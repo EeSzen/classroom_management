032b8c3bb12463cdc60b2530af5d3d0a7cb0decd
Ee Szen <eeszenlee@gmail.com>
2024-09-02 11:42:34 +0800

    update

--- index.php
+++ index.php
@@ -88,6 +88,9 @@ $database = new PDO(
       </div>
     </div>
 
+<?php
+echo "hello"
+?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
